c59e8e988ade96a1f244fb662e649f098385556a
Andre Bocchini <andrebocchini@gmail.com>
2019-07-06 07:36:09 -0500

    Add test for AccountRequest

--- SwiftYNAB/SwiftYNABTests/requests/AccountRequestTests.swift
+++ SwiftYNAB/SwiftYNABTests/requests/AccountRequestTests.swift
@@ -30,4 +30,12 @@ class AccountsRequestTests: XCTestCase {
         XCTAssertNil(request.body)
     }
 
+    func testAccount() {
+        let request = AccountRequest(budgetId: "43dcbde6-ccf4-4367-9d13-d6d7e9beeb99", accountId: "f9574c30-0614-7880-e926-0ae2c2f6d990")
+        XCTAssertEqual(request.path, "/v1/budgets/43dcbde6-ccf4-4367-9d13-d6d7e9beeb99/accounts/f9574c30-0614-7880-e926-0ae2c2f6d990")
+        XCTAssertEqual(request.method, "GET")
+        XCTAssertNil(request.query)
+        XCTAssertNil(request.body)
+    }
+
 }
