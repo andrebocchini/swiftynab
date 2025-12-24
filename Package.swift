b1430fabc5162e8602becb8f235e40d278877444
Andre Bocchini <andre@andrebocchini.com>
2025-12-24 14:39:51 -0500

    Update swift toolchain version

--- Package.swift
+++ Package.swift
@@ -5,10 +5,10 @@ import PackageDescription
 let package = Package(
     name: "SwiftYNAB",
     platforms: [
-        .macOS(.v12), .iOS(.v15), .tvOS(.v15), .watchOS(.v8),
+        .macOS(.v12), .iOS(.v15), .tvOS(.v15), .watchOS(.v8)
     ],
     products: [
-        .library(name: "SwiftYNAB", targets: ["SwiftYNAB"]),
+        .library(name: "SwiftYNAB", targets: ["SwiftYNAB"])
     ],
     dependencies: [],
     targets: [
@@ -20,7 +20,7 @@ let package = Package(
         .testTarget(
             name: "SwiftYNABTests",
             dependencies: ["SwiftYNAB"]
-        ),
+        )
     ],
     swiftLanguageModes: [.v6]
 )
